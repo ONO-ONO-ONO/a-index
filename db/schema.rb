43489991e24b4802a527837f034a936ff8c3f637
ONO-ONO-ONO <yono@hiblead.co.jp>
2020-02-09 20:58:58 +0900

    animalsテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_09_094208) do
+
+  create_table "animals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "created_user"
+    t.integer "updated_user"
+    t.string "name", null: false
+    t.string "binomial_name"
+    t.string "class"
+    t.string "order"
+    t.string "family"
+    t.string "genus"
+    t.string "species"
+    t.decimal "length", precision: 10
+    t.decimal "weight", precision: 10
+  end
 
 end
