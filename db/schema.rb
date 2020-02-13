b60d628e344c8fd1d10c90da3159f7e619c8e4d2
ONO-ONO-ONO <yono@hiblead.co.jp>
2020-02-13 12:07:05 +0900

    userテーブルのpassword_digestカラムno PRIMARYをoffに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_070823) do
+ActiveRecord::Schema.define(version: 2020_02_13_024811) do
 
   create_table "animals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_070823) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "user_name", null: false
     t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "password_digest"
     t.string "role", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
