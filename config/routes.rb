86fdd0bc8d0c96c31c93ebca1e121af291b097ef
ONO-ONO-ONO <yono@hiblead.co.jp>
2020-02-14 16:43:45 +0900

    色々追加(gem"n18i追加")

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "home/top" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlz
+  get "home/" => "home#top"
+
+  resources :animals
+
 end
