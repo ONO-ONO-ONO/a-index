6a91120f65c72f72c6822b0ea89aa8cc0b829624
ONO-ONO-ONO <yono@hiblead.co.jp>
2020-09-08 19:03:02 +0900

    デーモン化

--- config/puma.rb
+++ config/puma.rb
@@ -37,4 +37,15 @@ plugin :tmp_restart
 rails_env = ENV.fetch("RAILS_ENV")
 if rails_env == "production"
   bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
+  rails_root = Dir.pwd
+  bind "unix://" + File.join(rails_root, 'tmp', 'sockets', 'puma.sock')
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  stdout_redirect(
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
+  )
+daemonize
 end
