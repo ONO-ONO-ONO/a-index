695725fb979503273f693d272840670662b9b0e3
ONO-ONO-ONO <yono@hiblead.co.jp>
2020-09-23 00:29:05 +0900

    本番環境のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,32 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "a-index_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  # メイラーのテンプレートでフラグメントキャッシュを有効にするべきかどうかを指定します
+  config.action_mailer.perform_caching = true
+
+  # メール配信に失敗した場合にエラーを発生するかどうかを指定。
+  # trueにしておくことで、送信元のメールboxに送信エラーとなったメールがそのまま届きます
+  config.action_mailer.raise_delivery_errors = true
+
+  # deviseが認証用のURLなどを生成するのに必要になる（らしい）
+  config.action_mailer.default_url_options = { :host => 'http://http://153.127.39.117/' }
+
+  # 送信方法を指定（この他に:sendmail/:file/:testなどがあります)
+  config.action_mailer.delivery_method = :smtp
+
+  # 送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
+  config.action_mailer.smtp_settings = {
+    # gmail利用時はaddress,domain,portは下記で固定
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    # gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+    user_name: "animalndex2020",
+    # gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+    password: "yrdu qiek auld nhos",
+    # パスワードをBase64でエンコード
+    authentication: :login
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
